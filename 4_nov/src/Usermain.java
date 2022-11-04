fad6800db1359fd7ed72939bb8cffcb7cbc594d9
onkar <onkar@DESKTOP-OQFL1KJ>
2022-11-04 23:04:04 +0530

    version3

---
+++ 4_nov/src/Usermain.java
@@ -0,0 +1,18 @@
+import java .util.Scanner;
+public class Usermain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x=90;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  user Id");
+         int id=sc.nextInt();
+         sc.nextLine();
+         System.out.println("Enter the user Name");
+         String name=sc.nextLine();
+         System.out.println(id +"+name");
+         
+        
+	}
+
+}
