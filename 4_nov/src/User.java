2cdb24172c35092c4e9007e7b8419769d7509c1e
onkar <onkar@DESKTOP-OQFL1KJ>
2022-11-04 22:08:22 +0530

    version2

---
+++ 4_nov/src/User.java
@@ -0,0 +1,24 @@
+
+	public class User {
+		int userid;
+		String userName;
+		
+		User(int uid,String uName){
+			userid=uid;
+			userName=uName;
+		}
+		public void setUser(int urid,String urName) {
+			userid=urid;
+			userName=urName;
+			System.out.println("user Id is :"+userid);
+			System.out.println("user Name is :"+userName);
+		}
+	    public void getUser() {
+			return;
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		System.out.println("gauri");
+	}
+
+	}
